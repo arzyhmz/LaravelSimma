7bb84d933e4c8fab478e781b83d789148d13aaea
Imam Asari <imam.as@treeclouds.com>
2023-04-01 13:03:15 +0700

    use job to do synchronous

--- app/Http/Controllers/API/QontactSimmaController.php
+++ app/Http/Controllers/API/QontactSimmaController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 use App\Repositories\contactRepository;
 use Illuminate\Support\Facades\Http;
 use App\Models\contact;
- 
+use App\Jobs\GetDetailContactFromSimma;
+
 
 
 class QontactSimmaController extends Controller{
@@ -44,46 +45,48 @@ class QontactSimmaController extends Controller{
     public function list_change_simma_detail(Request $request){
         // get list chage save to database
         // can direct hit every data from changment and save detail tod atabase
+        // 
         $datas = $this->contactRepository->allquery()
-            ->limit(5)
             ->where('status', 'need_to_sync_detail')->get();
-
-        foreach ($datas as $data) {
-            $data['table_name'];
-            $token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMyNjUsImlzcyI6Imh0dHBzOlwvXC9leHBsdXNtb2JpbGUud29ybGR2aXNpb24ub3JnLnBoXC9leHBsdXMtbW9iaWxlXC9kZXZlbG9wZXJcL2xhcmF2ZWwtYmFja2VuZFwvcHVibGljXC9hcGlcL2F1dGhlbnRpY2F0ZSIsImlhdCI6MTUyNjUzMzkzNywiZXhwIjoxNTI2NTM3NTM3LCJuYmYiOjE1MjY1MzM5MzcsImp0aSI6IjMyNzE0ZmExYjk4OWFjMGFkMTdhYjkyZGQ4NDY3MmRjIn0.rJP7aBrteIFrtwzXBsBIu2jyhLQFkdPmOb8cDc9hEVM";
-            $response = Http::withHeaders([
-                'Authorization' => $token,
-            ])->post('simma.wahanavisi.org/laravel/public/v2/changes-wab', [
-                "TableName"=> $data['table_name'],
-                "TableID"=> $data['table_id']
-            ]);
-            $response = $response->json();
-            $response = $response[0];
-            $payload = array(
-                "name"=>$response["first_name"].' '.$response["last_name"],
-                // 'contact_email' => $response["Contact Email"],
-                'phone_number' => $response["phone_number"],
-                // 'status' => $response["Status"],
-                'date_of_birth'  => $response["date_of_birth"],
-                'source' => $response["source"],
-                'sponsor_id' => $response["partner_id"],
-                // 'name_see' => $response["Nama SEE"],
-                'motivation_code' => $response["motivation_code"],
-                'join_date' => $response["join_date"],
-                // 'sp' => $response["SP"],
-                'title' => $response["title"],
-                'partner_id' => $response["partner_id"],
-                // 'en' => $response["EN"],
-                // 'pl' => $response["PL"],
-                // 'dr' => $response["DR"],
-                'email_sponsor' => $response["email_sponsor"],
-                // 'need_tp_post' => 'true',
-                'status' => 'need_to_post',
-            );
-            $data->update($payload);
-        }
-
-        return response()->json(['message'=>'Berhasil tambah data', 'status'=>'success']);
+        GetDetailContactFromSimma::dispatch($datas);
+        // foreach ($datas as $data) {
+        //     $data['table_name'];
+        //     $token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMyNjUsImlzcyI6Imh0dHBzOlwvXC9leHBsdXNtb2JpbGUud29ybGR2aXNpb24ub3JnLnBoXC9leHBsdXMtbW9iaWxlXC9kZXZlbG9wZXJcL2xhcmF2ZWwtYmFja2VuZFwvcHVibGljXC9hcGlcL2F1dGhlbnRpY2F0ZSIsImlhdCI6MTUyNjUzMzkzNywiZXhwIjoxNTI2NTM3NTM3LCJuYmYiOjE1MjY1MzM5MzcsImp0aSI6IjMyNzE0ZmExYjk4OWFjMGFkMTdhYjkyZGQ4NDY3MmRjIn0.rJP7aBrteIFrtwzXBsBIu2jyhLQFkdPmOb8cDc9hEVM";
+        //     $response = Http::withHeaders([
+        //         'Authorization' => $token,
+        //     ])->post('simma.wahanavisi.org/laravel/public/v2/changes-wab', [
+        //         "TableName"=> $data['table_name'],
+        //         "TableID"=> $data['table_id']
+        //     ]);
+        //     $response = $response->json();
+        //     $response = $response[0];
+        //     $payload = array(
+        //         "name"=>$response["first_name"].' '.$response["last_name"],
+        //         // 'contact_email' => $response["Contact Email"],
+        //         'phone_number' => $response["phone_number"],
+        //         // 'status' => $response["Status"],
+        //         'date_of_birth'  => $response["date_of_birth"],
+        //         'source' => $response["source"],
+        //         'sponsor_id' => $response["partner_id"],
+        //         // 'name_see' => $response["Nama SEE"],
+        //         'motivation_code' => $response["motivation_code"],
+        //         'join_date' => $response["join_date"],
+        //         // 'sp' => $response["SP"],
+        //         'title' => $response["title"],
+        //         'partner_id' => $response["partner_id"],
+        //         // 'en' => $response["EN"],
+        //         // 'pl' => $response["PL"],
+        //         // 'dr' => $response["DR"],
+        //         'email_sponsor' => $response["email_sponsor"],
+        //         // 'need_tp_post' => 'true',
+        //         'status' => 'need_to_post',
+        //     );
+        //     $data->update($payload);
+        // }
+        return response()->json([
+            'message'=>'Process run in background, please wait the progress', 
+            'status'=>'success'
+        ]);
     }
 
     public function post_contact_to_qontact(Request $request){
---
+++ app/Jobs/GetDetailContactFromSimma.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+
+class GetDetailContactFromSimma implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $datas;
+
+    public function __construct($datas)
+    {
+        $this->datas = $datas;
+    }
+
+    public function handle(){
+        $i = 0;
+        foreach ($this->datas as $data) {
+            $data['table_name'];
+            $token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMyNjUsImlzcyI6Imh0dHBzOlwvXC9leHBsdXNtb2JpbGUud29ybGR2aXNpb24ub3JnLnBoXC9leHBsdXMtbW9iaWxlXC9kZXZlbG9wZXJcL2xhcmF2ZWwtYmFja2VuZFwvcHVibGljXC9hcGlcL2F1dGhlbnRpY2F0ZSIsImlhdCI6MTUyNjUzMzkzNywiZXhwIjoxNTI2NTM3NTM3LCJuYmYiOjE1MjY1MzM5MzcsImp0aSI6IjMyNzE0ZmExYjk4OWFjMGFkMTdhYjkyZGQ4NDY3MmRjIn0.rJP7aBrteIFrtwzXBsBIu2jyhLQFkdPmOb8cDc9hEVM";
+            $response = Http::withHeaders([
+                'Authorization' => $token,
+            ])->post('simma.wahanavisi.org/laravel/public/v2/changes-wab', [
+                "TableName"=> $data['table_name'],
+                "TableID"=> $data['table_id']
+            ]);
+            $response = $response->json();
+            if ($response) {
+                $response = $response[0];
+                $payload = array(
+                    "name"=>$response["first_name"].' '.$response["last_name"],
+                    // 'contact_email' => $response["Contact Email"],
+                    'phone_number' => $response["phone_number"],
+                    // 'status' => $response["Status"],
+                    'date_of_birth'  => $response["date_of_birth"],
+                    'source' => $response["source"],
+                    'sponsor_id' => $response["partner_id"],
+                    // 'name_see' => $response["Nama SEE"],
+                    'motivation_code' => $response["motivation_code"],
+                    'join_date' => $response["join_date"],
+                    // 'sp' => $response["SP"],
+                    'title' => $response["title"],
+                    'partner_id' => $response["partner_id"],
+                    // 'en' => $response["EN"],
+                    // 'pl' => $response["PL"],
+                    // 'dr' => $response["DR"],
+                    'email_sponsor' => $response["email_sponsor"],
+                    // 'need_tp_post' => 'true',
+                    'status' => 'need_to_post',
+                );
+                $data->update($payload);
+            }
+            $i++;
+            if ($i % 10 == 0) {
+                sleep(5);
+            }
+        }
+    }
+}
